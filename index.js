4fface9729ae1401135d5ee3e3661e7a0c899c67
KaylaTan91 <kaylatan91@gmail.com>
2023-08-22 14:52:35 -0400

    Finished workshop

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import the pets array from data.js
 const pets = require('./data');
 
-// init express app
+// init express apps
 const express = require('express');
 const app = express();
 
@@ -21,31 +21,28 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets)
 });
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
+app.get('/api/v1/pets/:owner', (req, res) => {
     // get the owner from the request
-
-
+    const {owner} = req.params
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
-
     // send the pet as a response
+    res.send(pet)
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
-
+    const {name} = req.params
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
-
     // send the pet as a response
-
+    res.send(pet)
 });
 
 app.listen(PORT, () => {
